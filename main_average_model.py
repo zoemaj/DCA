e5c5596d09a23322da6c9bc96e895cc1d490cf75
Zoé <61463193+zoemaj@users.noreply.github.com>
2024-03-07 14:22:19 +0100

    Add files via upload

--- main_average_model.py
+++ main_average_model.py
@@ -2,10 +2,8 @@ import argparse
 import average_model
 
 if __name__=="__main__":
-    
     parser=argparse.ArgumentParser()
-    parser.add_argument("model_name",help="The name of the models to average (without the index number _i).")
-    parser.add_argument("n_models",help="The number of models to average.")
+    parser.add_argument("model_name",help="The name of the model to average without the '_number'.")
+    parser.add_argument("n_model", help="How many models we want to average (they should exist).")
     args=parser.parse_args()
-    
-    average_model.compute_average(args.model_name,args.n_models)
+    average_model.average_model(args.model_name, args.n_model)
