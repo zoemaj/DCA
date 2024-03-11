b983235bac3551f0b5c60d715383910845387e90
Zoé <61463193+zoemaj@users.noreply.github.com>
2024-03-11 21:16:38 +0100

    Add files via upload

--- main_weights.py
+++ main_weights.py
@@ -4,9 +4,9 @@ import weights
 if __name__=="__main__":
     
     parser=argparse.ArgumentParser()
-    parser.add_argument("input_name",help="Thie input MSA preprocessed using the file execute_preprocessing.")
-    parser.add_argument("threshold", help="The percentage of simulutude accepted.")
-    parser.add_argument("output_name", help="Name for the output file.")
+    parser.add_argument("input_name",help="Thie input MSA preprocessed using the file execute_preprocessing.", type=str)
+    parser.add_argument("output_name", help="Name for the output file.", type=str)
+    parser.add_argument("-threshold", help="The percentage of simulutude accepted. Default=0.8, we keep all the sequences", default=0.8, type=float)
     args=parser.parse_args()
     
-    weights.weights(args.input_name, args.threshold, args.output_name)
+    weights.weights(args.input_name, args.output_name, args.threshold)
--- weights.py
+++ weights.py
@@ -3,34 +3,48 @@ import numba
 from numba import jit
 import os
 
-def weights(input_file, threshold, output_file) :
-    """
-    load the MSA from the input file, compute the weight for each sequence and write them in the output file
-    :param input_file: name of the input file containing the MSA preprocessed using "execute_preprocessing.py"
-    :type input_file: string
-    :param output_file: name of the output file
-    :type output_file: string
-    :return: nothing
-    :rtype: None
-    """
+def weights(input_file, output_file, threshold=0.8) :
+    '''
+    This function write the weights of the different sequences in the MSA 
+    input:
+        input_file  ->  name of the input file containing the MSA preprocessed
+                        string
+        threshold   ->  threshold of similarity to consider two sequences as similar (percentage of identical amino acids in the same position in the two sequences)
+                        float
+        output_file ->  name of the output file
+                        string
+    '''
     #load the MSA in the input file
     MSA = np.genfromtxt(input_file, delimiter=',')
-    #check if the folder exists
+    #check if the folder exists and create it if not
     if not os.path.exists(os.path.dirname(output_file)):
         #create it
         os.makedirs(os.path.dirname(output_file))
+    #check if the file already exists
+    else:
+        try:
+            with open(output_file, "r") as file: #"r" is for read
+                #ask if we want to overwrite it
+                overwrite=input("Do you want to overwrite it? (yes/no)")
+                while overwrite!="yes" and overwrite!="no":
+                    input("Please enter yes or no")
+                if overwrite=="no":
+                    return
+        except:
+            pass
     #write the weight in the output file
-    np.savetxt(output_file, get_weights(MSA,float(threshold)))
+    np.savetxt(output_file, get_weights(MSA,threshold))
 
 @jit(nopython=True, parallel=True) #parallelise using numba
 def get_weights(MSA,threshold) :
     """
-    compute the weight of each sequence of the MSA given in paramter
-
-    :param MSA: preprocessed MSA
-    :type MSA: numpy array
-    :return: weights of each sequence
-    :rtype: numpy array
+    This function compute the weight of each sequence given the threshold of similarity.
+    input:  
+        MSA         ->  table of sequences
+                        csv file, shape (N,L) with N the number of homologous sequences and L the length of a sequence
+        threshold   ->  percentage of similarity between two sequences considered as identical
+                        two sequences are the same if (number of identical a.a at the good position)/L > threshold 
+                        float
     """
     weights = np.zeros(len(MSA))
     for i, seq in enumerate(MSA) :
@@ -41,18 +55,19 @@ def get_weights(MSA,threshold) :
 @jit(nopython=True) #parallelise using numba
 def weight(seq, MSA, threshold) :
     """
-    compute the weight of the sequence seq given the MSA and return the weight
-
-    :param seq: sequence of the MSA for which we want to compute the weight
-    :type seq: numpy array
-    :param MSA: MSA to which the sequence belong
-    :type seq: numpy array
-    :return: weight of the sequence
-    :rtype: numpy array
+    This function will, according to the threshold, computes the weight of a sequence with the others in the MSA
+    input:
+        seq         ->  the seq from which we want to extract the weight
+                        sequence of int numbers corresponding to the different a.a
+        MSA         ->  table of sequences
+                        csv file, shape (N,L) with N the number of homologous sequences and L the length of a sequence
+        threshold   ->  percentage of similarity between two sequences considered as identical
+                        two sequences are the same if (number of identical a.a at the good position)/L > threshold 
+                        float
+    output:
+        weight      -> 1/(number of similar sequences according to the threshold)
+                        float 
     """
-    #we compute the weight with a threshold of 80% similarity
-    #modif add 27-02-24. BEFORE only this: return 1/(np.sum(np.sum(seq == MSA, axis=1)/len(seq) > threshold))
-    #check that the sum is not 0
     N= np.sum(np.sum(seq == MSA, axis=1)/len(seq) > threshold)
     if N == 0 :
         return 1.0
