b1cd0c35bdff88c471302ce968e36f6e4c3b6d0a
Zoé <61463193+zoemaj@users.noreply.github.com>
2024-04-10 11:35:44 +0200

    Add files via upload

--- main_preprocessing.py
+++ main_preprocessing.py
@@ -5,7 +5,7 @@ if __name__=="__main__":
     
     parser=argparse.ArgumentParser()
     parser.add_argument("input_name",help="The input MSA in fasta format or csv.", type=str)
-    parser.add_argument("-output_name", help="Name for the output file. Default=path(input_name)/preprocessing_(threshold)gaps/preprocessed_(threshold)gaps.csv", default="",type=str)
+    parser.add_argument("-output_name", help="Name for the output file. Default=path(input_name)/preprocessing-(threshold)gaps/preprocessed-(threshold)gaps.csv", default="",type=str)
     parser.add_argument("-threshold",help="The threshold for the percentage of gaps in a sequence. Default=1.0", default=1.0, type=float)
     args=parser.parse_args()
     
