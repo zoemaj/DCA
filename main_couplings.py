dd5a1868493c41a1a04a0c470a7016f301d49218
Zoé <61463193+zoemaj@users.noreply.github.com>
2024-04-10 14:36:22 +0200

    Add files via upload

--- main_couplings.py
+++ main_couplings.py
@@ -1,12 +1,10 @@
 import argparse
-import couplings
+import couplings_NEW as couplings
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser()
     #parser.add_argument("triplets_name",help="File containing the triplets of the NN obtained using model.py.")
     parser.add_argument("model_name", help="Name of the file where the trained model was saved", type=str)
-    parser.add_argument("L", help="length of the sequences in the MSA", type=int)
-    parser.add_argument("K", help="number of categories in the MSA", type=int)
     parser.add_argument("-number_models", help="number of model to train. Default=1", default=1, type=int)
     parser.add_argument("-type_average", help= "if number_models this is neglected. Otherwise you need to choose between 'average_couplings' or 'average_couplings_frob' with respectively do the average for each couplings of each model, do the average for each couplings and frobenius of each model. Default='average_couplings'", default="average_couplings", type=str)
     parser.add_argument("-output_name", help="Name for the output file that will containg the couplings. Default=path(model_name)/<type_average>/couplings", default="/", type=str)
@@ -15,4 +13,4 @@ if __name__=="__main__":
     parser.add_argument("-model_type", help="type of the model, can be 'linear', 'non-linear' or 'mix'", default="linear", type=str)
     args=parser.parse_args()
     
-    couplings.couplings(args.model_name, args.L, args.K, args.number_models, args.type_average, args.output_name, args.figure, args.data_per_col, args.model_type)
+    couplings.couplings(args.model_name, args.number_models, args.type_average, args.output_name, args.figure, args.data_per_col, args.model_type)
